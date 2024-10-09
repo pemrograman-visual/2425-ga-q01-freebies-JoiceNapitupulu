cfb5b03b51d413c03b20a0254227e964f1e7ace9
JoiceNapitupulu <anastasyajoicen@gmail.com>
2024-10-09 15:59:41 +0700

    kami telah menyelesaikan tugas

--- X01.java
+++ X01.java
@@ -1,2 +1,38 @@
-// NIM - Your Name
-// NIM - Your Name
+// 12S24014 - Arion Dippos Pandapotan Manurung
+// 12S24020 - Joice Anastasya Napitupulu
+
+import java.util.*;
+import java.lang.Math;
+
+public class X01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double hargabuku, hargaterkecil, hargasementara, hargatotal;
+
+        hargabuku = 0;
+        hargasementara = 0;
+        hargaterkecil = 1000;
+        do {
+            hargabuku = Double.parseDouble(input.nextLine());
+            if (hargabuku == 0) {
+            } else {
+                if (hargaterkecil < hargabuku) {
+                } else {
+                    hargaterkecil = hargabuku;
+                }
+                hargasementara = hargasementara + hargabuku;
+            }
+        } while (hargabuku != 0);
+        if (hargasementara >= 100) {
+            hargatotal = hargasementara - hargaterkecil;
+        } else {
+            hargatotal = hargasementara;
+        }
+        System.out.println(toFixed(hargatotal,2));
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
